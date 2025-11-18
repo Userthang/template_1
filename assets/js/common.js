3ed7a8adde122725ead298d033493b6ea6a358bb
Userthang <157868790+Userthang@users.noreply.github.com>
2025-11-18 22:55:52 +0700

    a

--- assets/js/common.js
+++ assets/js/common.js
@@ -14,3 +14,44 @@ function show_content(tabId, select_tab) {
         activeTab.classList.add('active');
     }
 }
+
+function toggle_menu_info() {
+    const header_info = document.getElementById('header_info');
+    header_info.classList.toggle('show');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const open_menu = document.querySelector('.open_menu');
+    const header_nav = document.querySelector('.header_nav');
+    const overlay = document.querySelector('.overlay');
+
+    open_menu.addEventListener('click', function () {
+        open_menu.classList.toggle('active');
+        header_nav.classList.toggle('show');
+        overlay.classList.toggle('active');
+
+    });
+
+    overlay.addEventListener('click', function () {
+        open_menu.classList.remove('active');
+        header_nav.classList.remove('show');
+        overlay.classList.remove('active');
+        document.body.style.overflow = '';
+    });
+
+});
+
+function get_height(get_value, replace_value) {
+    const source = document.querySelector(get_value)
+    const target = document.querySelector(replace_value);
+    
+    target.style.height = source.offsetHeight + 'px';
+    new ResizeObserver(() => {
+        target.style.height = source.offsetHeight + 'px';
+    }).observe(source);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+        get_height('.content_voice', '.replace_content_voice');
+        get_height('.content_staff', '.replace_content_staff');
+    });
